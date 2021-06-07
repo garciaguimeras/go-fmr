d4c7b8e553138aa457e82cf0fc7ec24507654c54
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2021-06-07 08:04:02 -0400

    Fix comments

--- fmr/slice.go
+++ fmr/slice.go
@@ -1,22 +1,17 @@
-// Provides filter, map and reduce functions to use with Go slices
+// Package fmr provides filter, map and reduce functions to use with Go slices
 package fmr
 
-// Filter function
 type FilterFunction func(elem interface{}) bool
-
-// Map function
 type MapFunction func(elem interface{}) interface{}
-
-// Reduce function
 type ReduceFunction func(elem1 interface{}, elem2 interface{}) interface{}
 
-// Contains the slice data and the function chain to be applied
+// Slice contains the slice data and the function chain to be applied
 type Slice struct {
 	data      interface{}
 	functions []interface{}
 }
 
-// Sets the slice to be iterated
+// SetSlice sets the slice to be iterated
 func SetSlice(data interface{}) *Slice {
 	return &Slice{
 		data:      data,
@@ -24,25 +19,25 @@ func SetSlice(data interface{}) *Slice {
 	}
 }
 
-// Adds a filter function to the chain
+// Filter adds a filter function to the function chain
 func (s *Slice) Filter(f FilterFunction) *Slice {
 	s.functions = append(s.functions, f)
 	return s
 }
 
-// Adds a map function to the chain
+// Map adds a map function to the function chain
 func (s *Slice) Map(f MapFunction) *Slice {
 	s.functions = append(s.functions, f)
 	return s
 }
 
-// Adds a reduce function to the chain
+// Reduce adds a reduce function to the function chain
 func (s *Slice) Reduce(f ReduceFunction) *Slice {
 	s.functions = append(s.functions, f)
 	return s
 }
 
-// Runs the filter-map-reduce chain and gets a result
+// Get runs the filter-map-reduce chain and gets a result
 func (s *Slice) Get() (interface{}, error) {
 	// Creates a new context to apply the function chain on the data
 	ctx := createContext(s.data)
